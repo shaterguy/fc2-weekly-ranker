a275e6419f5ab61b6d5662e91404f42dc199c331
SHatergUY <108107617+shaterguy@users.noreply.github.com>
2026-08-31 15:05:36 +0900

    chore: bump TEST version to 0.1.0-dev9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.shaterguy.fc2weeklyranker.dev"
         minSdk = 29
         targetSdk = 36
-        versionCode = 8
-        versionName = "0.1.0-dev8"
+        versionCode = 9
+        versionName = "0.1.0-dev9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

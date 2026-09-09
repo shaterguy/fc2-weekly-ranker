3aec9b4cc1d8850c5df3a689a64c80f0c2035bcb
SHatergUY <108107617+shaterguy@users.noreply.github.com>
2026-09-09 10:51:52 +0900

    dev17: bump versionCode above bad dev16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.shaterguy.fc2weeklyranker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 40
+        versionCode = 41
         versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }

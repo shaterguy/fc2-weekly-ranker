402d7b4287d1c90e8f9da8f6bdab693762af693d
SHatergUY <108107617+shaterguy@users.noreply.github.com>
2026-09-14 15:13:42 +0900

    build: advance TEST lineage to dev30

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.shaterguy.fc2weeklyranker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 53
+        versionCode = 54
         versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -20,7 +20,7 @@ android {
     buildTypes {
         debug {
             applicationIdSuffix = ".dev"
-            versionNameSuffix = "-dev29"
+            versionNameSuffix = "-dev30"
             signingConfig = null
         }
         release {

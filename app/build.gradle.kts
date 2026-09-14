6ab8c9286f481d0e292f2d81f2d06b838040b9be
SHatergUY <108107617+shaterguy@users.noreply.github.com>
2026-09-14 16:16:59 +0900

    build: bump dev version to 31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.shaterguy.fc2weeklyranker"
         minSdk = 29
         targetSdk = 36
-        versionCode = 54
+        versionCode = 55
         versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -20,7 +20,7 @@ android {
     buildTypes {
         debug {
             applicationIdSuffix = ".dev"
-            versionNameSuffix = "-dev30"
+            versionNameSuffix = "-dev31"
             signingConfig = null
         }
         release {

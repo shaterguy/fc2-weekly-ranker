d69c62b4e8f80944df466af9d44666d0376d9a46
SHatergUY <108107617+shaterguy@users.noreply.github.com>
2026-09-17 23:53:49 +0900

    feat: add system-linked Compose theme

---
+++ app/src/main/java/com/shaterguy/fc2weeklyranker/ui/theme/RankerTheme.kt
@@ -0,0 +1,15 @@
+package com.shaterguy.fc2weeklyranker.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RankerTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = if (isSystemInDarkTheme()) darkColorScheme() else lightColorScheme(),
+        content = content,
+    )
+}
